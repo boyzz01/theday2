5f3ff64ec664d2812f65dd6e7612afd833d68bed
Ardi Sahputra <167969030+ArdiSahputra@users.noreply.github.com>
2026-04-22 15:31:05 +0700

    feat: add template registry as single source of truth

---
+++ resources/js/Components/invitation/templates/registry.js
@@ -0,0 +1,6 @@
+// resources/js/Components/invitation/templates/registry.js
+import NusantaraTemplate from './NusantaraTemplate.vue'
+
+export const TEMPLATE_MAP = {
+    'nusantara': NusantaraTemplate,
+}
