5f07fd3634cf72cf8dd3db72259b2263c1f100a0
Ardi Sahputra <167969030+ArdiSahputra@users.noreply.github.com>
2026-04-25 16:13:50 +0700

    feat: add Beach, Garden, NightSky wrapper templates and register in TEMPLATE_MAP

---
+++ resources/js/Components/invitation/templates/NightSkyTemplate.vue
@@ -0,0 +1,17 @@
+<!-- resources/js/Components/invitation/templates/NightSkyTemplate.vue -->
+<script setup>
+import SceneTemplate  from './scene/SceneTemplate.vue'
+import NightSkyConfig from './scene/configs/NightSkyConfig.js'
+
+defineProps({
+    invitation: { type: Object,  required: true },
+    messages:   { type: Array,   default: () => [] },
+    guest:      { type: Object,  default: null },
+    isDemo:     { type: Boolean, default: false },
+    autoOpen:   { type: Boolean, default: false },
+})
+</script>
+
+<template>
+    <SceneTemplate v-bind="$props" :sceneConfig="NightSkyConfig" />
+</template>
--- resources/js/Components/invitation/templates/registry.js
+++ resources/js/Components/invitation/templates/registry.js
@@ -1,8 +1,14 @@
 // resources/js/Components/invitation/templates/registry.js
 import NusantaraTemplate from './NusantaraTemplate.vue'
-import PearlTemplate from './PearlTemplate.vue'
+import PearlTemplate     from './PearlTemplate.vue'
+import BeachTemplate     from './BeachTemplate.vue'
+import GardenTemplate    from './GardenTemplate.vue'
+import NightSkyTemplate  from './NightSkyTemplate.vue'
 
 export const TEMPLATE_MAP = {
     'nusantara': NusantaraTemplate,
-    'pearl': PearlTemplate,
+    'pearl':     PearlTemplate,
+    'beach':     BeachTemplate,
+    'garden':    GardenTemplate,
+    'night-sky': NightSkyTemplate,
 }
